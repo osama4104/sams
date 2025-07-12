4f50fb328d2c9388425cdd67b30e5fa60665005e
Aaradhya <aaradhyajoshi787@gmail.com>
2025-07-12 13:00:31 +0530

    add Login component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
-import './App.css'
-
+import "./App.css";
+import Login from "./components/Login";
 function App() {
   return (
     <>
       <h1>Smart Alarm Management System</h1>
+      <Login />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
