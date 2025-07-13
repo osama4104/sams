6c3a2f450ad8f9a1258bb9b45d746e90f53de8f7
Aaradhya <aaradhyajoshi787@gmail.com>
2025-07-13 18:18:06 +0530

    add component

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,10 +1,12 @@
 // routes/AppRoutes.tsx
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from '../components/Login';
-import NotFound from '../components/NotFound';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "../components/Login";
+import NotFound from "../components/NotFound";
+import Admin from "../components/Admin";
+import DashBoard from "../components/DashBoard";
 
-import MainLayout from '../layouts/MainLayout';
-import SignUp from '../components/SignUp';
+import MainLayout from "../layouts/MainLayout";
+import SignUp from "../components/SignUp";
 // import Dashboard from '../components/Dashboard';
 // import PatientList from '../components/PatientList';
 // import PatientDetail from '../components/PatientDetail';
@@ -12,23 +14,24 @@ import SignUp from '../components/SignUp';
 // import Profile from '../components/Profile';
 
 const AppRoutes = () => (
-   <Router>
-      <Routes>
-         <Route path="/login" element={<Login />} />
-         <Route path="/signup" element={<SignUp />} />
-
-         {/* Protected routes with layout */}
-         <Route element={<MainLayout />}>
-            {/* <Route path="/dashboard" element={<Dashboard />} />
+  <Router>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/admin" element={<Admin />} />
+      <Route path="/dashboard" element={<DashBoard />} />
+      {/* Protected routes with layout */}
+      <Route element={<MainLayout />}>
+        {/* <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/patients" element={<PatientList />} />
             <Route path="/patients/:id" element={<PatientDetail />} />
             <Route path="/alerts" element={<AlertCenter />} />
             <Route path="/profile" element={<Profile />} /> */}
-         </Route>
+      </Route>
 
-         <Route path="*" element={<NotFound />} />
-      </Routes>
-   </Router>
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </Router>
 );
 
 export default AppRoutes;
